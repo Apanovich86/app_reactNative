2546110799d99c07f568692b0e007650a8b2ba91
Apanovich86 <apanovich.anna.itec@gmail.com>
2021-09-14 23:49:56 +0300

    reactNative_bottomDrawer hide toolbar

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import Users from "./components/Users";
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
@@ -10,17 +10,17 @@ import UserDetails from "./screens/UserDetails";
 
 let StackNavigator = createStackNavigator();
 export default function App() {
-  return (
-      <NavigationContainer>
-          <StackNavigator.Navigator initialRouteName="Users page">
-              <StackNavigator.Screen name={'Home'} component={Home}/>
-              <StackNavigator.Screen name={'Users page'} component={Users}/>
-              <StackNavigator.Screen name={'uDetails'} component={UserDetails}/>
-          </StackNavigator.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <StackNavigator.Navigator initialRouteName="Users page">
+                <StackNavigator.Screen name={'Home'} component={Home}/>
+                <StackNavigator.Screen name={'Users page'} component={Users}/>
+                <StackNavigator.Screen name={'uDetails'} component={UserDetails} options={{title: 'uDetails', headerShown: false}}/>
+            </StackNavigator.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+    container: {},
 });
